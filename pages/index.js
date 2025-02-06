932d58288c227a7f3ab914351e70427808b346f1
Sachin-0001 <sachin.samprit@gmail.com>
2025-02-06 22:02:50 +0530

    head tag config

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import Head from "next/head";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -13,6 +13,12 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
+    <>
+      <Head>
+      <title>Hunting Coder</title>
+        <meta name="description" content="A blog for coders" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
@@ -109,6 +115,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
-    </div>
+      </div>
+      </>
   );
 }
