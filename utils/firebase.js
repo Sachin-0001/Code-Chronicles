e13ba83b2bf90d0486590b19884f89b58ae21bbb
Sachin-0001 <sachin.samprit@gmail.com>
2025-02-10 22:34:57 +0530

    hardcoded firebaseconfig

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDVN2SS62D8WtYo1iILt_u8p0Z0pL7H9iE",
+  authDomain: "code-chronicles-1adfa.firebaseapp.com",
+  projectId: "code-chronicles-1adfa",
+  storageBucket: "code-chronicles-1adfa.firebasestorage.app",
+  messagingSenderId: "524425951970",
+  appId: "1:524425951970:web:98a5e0c17ff07833873cbb",
+  measurementId: "G-7MMJTMRJ2R"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
